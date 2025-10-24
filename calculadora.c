968b6d8dd637652cc00a8662579e65cad7100d30
ANDRE NONAKA MADEIRA <nonaka.andre@gmail.com>
2025-10-24 11:48:26 +0000

    24/10

--- calculadora.c
+++ calculadora.c
@@ -4,7 +4,7 @@ double somar(double num_a, double num_b) {  /*float ocupa 32bits e double ocupa 
     return num_a + num_b;
 }
 
-double subtrai(double num_a, double num_b) {
+double subtrair(double num_a, double num_b) {
     return num_a - num_b;
 }
 
@@ -14,11 +14,28 @@ int main () {
    double resultado;
 
    printf("Digite um número:");
-   scanf("%lf", &num_1);
+   scanf("%lf", &num_1);   /*lf = long float*/
 
    printf("Digite um número:");
    scanf("%lf", &num_2);
 
-   
+   printf("Selecione a operação:\n");
+   printf("1 - Soma\n");
+   printf("2 - Subtrair\n");
+   printf(":");
+   scanf("%d", &operacao);
+
+   switch (operacao) {  /*o switch é uma estrutura menos utilizada. Ele só consegue avaliar igualdades*/
+      case 1:           /*O switch não consegue ler número decimais. É usado somente em int, string e char*/
+          resultado = somar(num_1, num_2);
+          break;   /*O break para a execução*/
+      case 2:
+          resultado = subtrair(num_1, num_2);
+          break;
+          default: /*É um opcional dentro do switch*/
+             printf("Opção invalida\n");    
+   }
+
+    printf("Resultado: %.2lf\n", resultado); /**/
     return 0;
 }
